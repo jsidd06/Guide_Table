de1f601a161d3671725b4d99fdc299b4d924c413
Siddharth <jsidd06@gmail.com>
2021-09-03 20:26:35 +0530

    spliting components multiple types

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Expensetion from "./components/Expensetion/Expensetion";
+import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
 
 function App() {
   const expenses = [
@@ -31,22 +31,22 @@ function App() {
   return (
     <div>
       <h2>Let's start</h2>
-      <Expensetion
+      <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
       />
-      <Expensetion
+      <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
       />
-      <Expensetion
+      <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
       />
-      <Expensetion
+      <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
