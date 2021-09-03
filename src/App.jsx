380feea57b4635c60f64dba70ed705cbfc9aa540
Siddharth <jsidd06@gmail.com>
2021-09-03 18:56:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Expensetion from './components/Expensetion/Expensetion'
+
+function App() {
+    return (
+        <div>
+            <h2>Let's start</h2>
+            <Expensetion />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Expensetion/Expensetion.jsx
@@ -0,0 +1,15 @@
+import './Expensetion.css'
+
+function Expensetion() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+      <div className="expense-item__price">$284.76</div>
+      </div>
+    </div>
+  );
+}
+
+export default Expensetion;
