4ad4dbce66866f3556ed75277127329dbecfc7a0
Siddharth <jsidd06@gmail.com>
2021-09-04 10:10:35 +0530

    composation use is components and use props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,38 @@
 import React from "react";
-import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
+import Expense from "./components/Expense/Expense";
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Total Paper",
-      amount: 94.12,
-      date: new Date(2021, 3, 9),
-    },
-    {
-      id: "e2",
-      title: "New Tv",
-      amount: 799.49,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 5, 29),
-    },
-    {
-      id: "e4",
-      title: "New desk (Wonden)",
-      amount: 450,
-      date: new Date(2021, 6, 30),
-    },
-  ];
+    const expenses = [
+        {
+          id: "e1",
+          title: "Total Paper",
+          amount: 94.12,
+          date: new Date(2021, 3, 9),
+        },
+        {
+          id: "e2",
+          title: "New Tv",
+          amount: 799.49,
+          date: new Date(2021, 2, 28),
+        },
+        {
+          id: "e3",
+          title: "Car Insurance",
+          amount: 294.67,
+          date: new Date(2021, 5, 29),
+        },
+        {
+          id: "e4",
+          title: "New desk (Wonden)",
+          amount: 450,
+          date: new Date(2021, 6, 30),
+        },
+      ];
+  
   return (
     <div>
       <h2>Let's start</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expense items={expenses}/>
     </div>
   );
 }
