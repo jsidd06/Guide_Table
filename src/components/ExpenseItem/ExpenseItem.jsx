49973a98c8129d5b6dd0be273e162038eb459170
Siddharth <jsidd06@gmail.com>
2021-09-05 12:51:51 +0530

    add a usestate to check it's working or not

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,21 +1,25 @@
-import Card from '../Card/Card';
-import ExpenseDate from '../ExpenseDate/ExpenseDate';
-import './ExpenseItem.css'
+import { useState } from "react";
+import Card from "../Card/Card";
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    const clickHandler = () => {
-      console.log('clicked')
-    }
+  const [title, setTitle] = useState(props.title)
+  const clickHandler = () => {
+    setTitle('updated');
+
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
-        <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-      <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler} >Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
