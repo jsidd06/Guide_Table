b055cba80ba062bb5bf48d009b38278d26120933
Siddharth <jsidd06@gmail.com>
2021-09-05 13:59:44 +0530

    add multiple states in working progress

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "../NewExpense/ExpenseForm.css";
 const ExpenseForm = () => {
-    const titleChangedHandler = (event) => {
-        console.log(event.target.value);
-    }
+    const [enterTitle, setEnterTitle] = useState("");
+    const [amount ,setAmount] = useState("");
+    const [date,setDate] = useState("");
+  const titleChangedHandler = (event) => {
+    setEnterTitle(event.target.value);
+  };
+  const amountChangedHandler = (event) => {
+      setAmount(event.target.value);
+  };
+  const dateChangedHandler = (event) => {
+      setDate(event.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
-          <div className="new-expense__control">
-              <label>Title</label>  
-              <input type="text" onChange={titleChangedHandler} /> 
-          </div>
-          <div className="new-expense__control">
-              <label>Amount</label>  
-              <input type="number" min='0.01' step='0.01'/> 
-          </div>
-          <div className="new-expense__control">
-              <label>Date</label>  
-              <input type="date" min='2019-01-01' max='2022-12-31'/> 
-          </div>
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangedHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            onChange={amountChangedHandler}
+            min="0.01"
+            step="0.01"
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            onChange={dateChangedHandler}
+            min="2019-01-01"
+            max="2022-12-31"
+          />
+        </div>
       </div>
       <div className="new-expense__actions">
-          <button type='submit'>Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
