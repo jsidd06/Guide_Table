f0ea37fa46036b090d63af0e796d0c0660341667
Siddharth <jsidd06@gmail.com>
2021-09-06 08:46:06 +0530

    adding a data on console log using usestate

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -22,18 +22,22 @@ const ExpenseForm = () => {
       date: new Date(enterDate),
     }
     console.log(expenseData);
+    setEnterTitle('');
+    setEnterAmount('');
+    setEnterDate('');
   }
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangedHandler} />
+          <input type="text" value={enterTitle} onChange={titleChangedHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enterAmount}
             onChange={amountChangedHandler}
             min="0.01"
             step="0.01"
@@ -43,6 +47,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enterDate}
             onChange={dateChangedHandler}
             min="2019-01-01"
             max="2022-12-31"
